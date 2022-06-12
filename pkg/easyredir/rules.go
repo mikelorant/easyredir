0c34768f685ac342cb9887fd25d7d1c951bcf500
Michael Lorant <michael.lorant@fairfaxmedia.com.au>
2022-06-12 12:46:05 +1000

    Path query

--- pkg/easyredir/rules.go
+++ pkg/easyredir/rules.go
@@ -3,6 +3,7 @@ package easyredir
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 type Rules struct {
@@ -35,28 +36,16 @@ type Links struct {
 	Prev string `json:"prev"`
 }
 
-type RulesOptions struct {
-	sourceFilter string
-	targetFilter string
-	limit        int
+type Pagination struct {
+	startingAfter	string
+	endingBefore	string
 }
 
-func (e *Easyredir) ListRules(opts ...func(*RulesOptions)) (r Rules, err error) {
-	options := &RulesOptions{}
-	for _, o := range opts {
-		o(options)
-	}
-
-	reader, err := e.client.sendRequest(e.config.BaseURL, "/rules", http.MethodGet, nil)
-	if err != nil {
-		return r, fmt.Errorf("unable to send request: %w", err)
-	}
-
-	if err := decodeJSON(reader, &r); err != nil {
-		return r, fmt.Errorf("unable to get json: %w", err)
-	}
-
-	return r, nil
+type RulesOptions struct {
+	sourceFilter 	string
+	targetFilter 	string
+	limit        	int
+	pagination		Pagination
 }
 
 func WithSourceFilter(url string) func(*RulesOptions) {
@@ -75,4 +64,56 @@ func WithLimit(limit int) func(*RulesOptions) {
 	return func(o *RulesOptions) {
 		o.limit = limit
 	}
+}
+
+func (e *Easyredir) ListRules(opts ...func(*RulesOptions)) (r Rules, err error) {
+	options := &RulesOptions{}
+	for _, o := range opts {
+		o(options)
+	}
+
+	pathQuery := listRulesPathQuery(options)
+	reader, err := e.client.sendRequest(e.config.baseURL, pathQuery, http.MethodGet, nil)
+	if err != nil {
+		return r, fmt.Errorf("unable to send request: %w", err)
+	}
+
+	if err := decodeJSON(reader, &r); err != nil {
+		return r, fmt.Errorf("unable to get json: %w", err)
+	}
+
+	return r, nil
+}
+
+func listRulesPathQuery(opts *RulesOptions) string {
+	var sb strings.Builder
+	var params []string
+
+	fmt.Fprint(&sb, "/rules")
+
+	if opts.pagination.startingAfter != "" {
+		params = append(params, fmt.Sprintf("starting_after=%v", opts.pagination.startingAfter))
+	}
+
+	if opts.pagination.endingBefore != "" {
+		params = append(params, fmt.Sprintf("ending_before=%v", opts.pagination.endingBefore))
+	}
+
+	if opts.sourceFilter != "" {
+		params = append(params, fmt.Sprintf("sq=%v", opts.sourceFilter))
+	}
+
+	if opts.targetFilter != "" {
+		params = append(params, fmt.Sprintf("tq=%v", opts.targetFilter))
+	}
+
+	if opts.limit != 0 {
+		params = append(params, fmt.Sprintf("limit=%v", opts.limit))
+	}
+
+	if len(params) != 0 {
+		fmt.Fprintf(&sb, "?%v", strings.Join(params, "&"))
+	}
+
+	return sb.String()
 }
--- pkg/easyredir/rules_test.go
+++ pkg/easyredir/rules_test.go
@@ -190,3 +190,116 @@ func TestListRules(t *testing.T) {
 		})
 	}
 }
+
+func TestListRulesPathQuery(t *testing.T) {
+	type Args struct {
+		options	*RulesOptions
+	}
+
+	type Want struct {
+		pathQuery string
+	}
+
+	tests := []struct{
+		name	string
+		args	Args
+		want	Want
+	}{
+		{
+			name: "no_options",
+			args: Args{
+				options: &RulesOptions{},
+			},
+			want: Want{
+				pathQuery: "/rules",
+			},
+		}, {
+			name: "starting_after",
+			args: Args{
+				options: &RulesOptions{
+					pagination: Pagination{
+						startingAfter: "96b30ce8-6331-4c18-ae49-4155c3a2136c",
+					},
+				},
+			},
+			want: Want{
+				pathQuery: "/rules?starting_after=96b30ce8-6331-4c18-ae49-4155c3a2136c",
+			},
+		}, {
+			name: "ending_before",
+			args: Args{
+				options: &RulesOptions{
+					pagination: Pagination{
+						endingBefore: "c6312a3c5514-94ea-81c4-1336-8ec03b69",
+					},
+				},
+			},
+			want: Want{
+				pathQuery: "/rules?ending_before=c6312a3c5514-94ea-81c4-1336-8ec03b69",
+			},
+		}, {
+			name: "source_filter",
+			args: Args{
+				options: &RulesOptions{
+					sourceFilter: "http://www1.example.org",
+				},
+			},
+			want: Want{
+				pathQuery: "/rules?sq=http://www1.example.org",
+			},
+		}, {
+			name: "target_filter",
+			args: Args{
+				options: &RulesOptions{
+					targetFilter: "http://www2.example.org",
+				},
+			},
+			want: Want{
+				pathQuery: "/rules?tq=http://www2.example.org",
+			},
+		}, {
+			name: "source_target_filter",
+			args: Args{
+				options: &RulesOptions{
+					sourceFilter: "http://www1.example.org",
+					targetFilter: "http://www2.example.org",
+				},
+			},
+			want: Want{
+				pathQuery: "/rules?sq=http://www1.example.org&tq=http://www2.example.org",
+			},
+		}, {
+			name: "limit",
+			args: Args{
+				options: &RulesOptions{
+					limit: 100,
+				},
+			},
+			want: Want{
+				pathQuery: "/rules?limit=100",
+			},
+		}, {
+			name: "all",
+			args: Args{
+				options: &RulesOptions{
+					sourceFilter: "http://www1.example.org",
+					targetFilter: "http://www2.example.org",
+					limit: 100,
+					pagination: Pagination{
+						startingAfter: "96b30ce8-6331-4c18-ae49-4155c3a2136c",
+					},
+				},
+			},
+			want: Want{
+				pathQuery: "/rules?starting_after=96b30ce8-6331-4c18-ae49-4155c3a2136c&sq=http://www1.example.org&tq=http://www2.example.org&limit=100",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := listRulesPathQuery(tt.args.options)
+			td.Cmp(t, got, tt.want.pathQuery)
+		})
+	}
+}
