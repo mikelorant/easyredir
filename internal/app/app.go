861fa6d4b9ada9fb4cb8f40a058085a92008c8ef
Michael Lorant <michael.lorant@fairfaxmedia.com.au>
2022-06-12 17:58:56 +1000

    Example run use

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,12 +1,27 @@
 package app
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/gotidy/ptr"
 	"github.com/mikelorant/easyredir-cli/pkg/easyredir"
 )
 
 func Run() error {
-	e := easyredir.New(&easyredir.Config{})
+	e := easyredir.New(&easyredir.Config{
+		APIKey:    os.Getenv("EASYREDIR_API_KEY"),
+		APISecret: os.Getenv("EASYREDIR_API_SECRET"),
+	})
 	e.Ping()
+	rules, err := e.ListRulesPaginator()
+	if err != nil {
+		return fmt.Errorf("unable to list rules: %w", err)
+	}
+
+	for _, r := range rules.Data {
+		fmt.Println(ptr.ToString(r.ID))
+	}
 
 	return nil
 }
